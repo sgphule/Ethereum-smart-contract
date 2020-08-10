368a0d2ae3263d486eed70fa123d64e5a01490c4
Sudarshan Phule <sudarshan.phule@hof-university.de>
2020-08-10 21:24:57 +0200

    First Smart Contract

---
+++ 1_HelloWorld.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.5.12;
+
+contract  ContractHello
+{
+	//STATE VARIABLES
+	string public message = "Hello World!";
+	//FUNCTIONS
+	function getMessage() public view returns(string memory)
+	{
+		return message;
+	}		
+}
